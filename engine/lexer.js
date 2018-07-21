53c15b55fc0da6d30ddfadeb0cd17c6001aaaa61
Jordan Bourne <jordanbourne@ymail.com>
2018-07-21 19:29:23 -0400

    Basic Lexer functionality implemented

--- engine/lexer.js
+++ engine/lexer.js
@@ -2,13 +2,149 @@ const fs = require("fs");
 
 class Lexer {
     constructor(params) {
-        this.content = params.content;
+        this.content = params.content.toString();
         this.options = params.options;
+        this.parsedContent = [];
     }
 
     renderContent(callback) {
-        return callback(null, this.content.toString().replace("<|title|>", this.options.title));
+        this.tokenizePage();
+        this.parsePageTokens();
+        return callback(null, this.parsedContent);
     }
+
+    tokenizePage() {
+        this.pos = 0;
+        this.bufferEnd = 0;
+        this.nextToken = "<|";
+        this.tokenizedContent = [];
+
+        while (this.pos <= this.content.length) {
+            this.bufferEnd = this.content.indexOf(this.nextToken, this.pos);
+            if (this.bufferEnd === -1) this.bufferEnd = this.content.length;
+
+            this.tokenizedContent.push({
+                token: this.nextToken === "<|" ? "HTML" : "JavaScript",
+                value: this.content.slice(this.pos, this.bufferEnd)
+            });
+
+            this.pos = this.bufferEnd + 2;
+            this.nextToken = this.nextToken === "<|" ? "|>" : "<|";
+        }
+    }
+
+    parsePageTokens() {
+        for (let i = 0; i < this.tokenizedContent.length; i++) {
+            let tokenValue = this.tokenizedContent[i].value;
+            if (this.tokenizedContent[i].token === "JavaScript") {
+                tokenValue = this.parseJavaScript(tokenValue);
+            }
+            this.parsedContent.push(tokenValue);
+        }
+
+        this.parsedContent = this.parsedContent.join("");
+    }
+
+    parseJavaScript(phrase) {
+        //hello + ' ' + world
+        this.tokenizeJavaScript(phrase);
+        this.parseTokenedJavascript();
+        return eval(this.parseTokenedJavascript());
+    }
+
+    tokenizeJavaScript(phrase) {
+        this.pos = 0;
+        this.bufferEnd = 0;
+        this.tokenizedJavaScript = [];
+
+        while(this.pos < phrase.length) {
+            this.bufferEnd = phrase.indexOf(" ", this.pos);
+            if (this.bufferEnd === -1) this.bufferEnd = phrase.length;
+
+            let tokenValue = phrase.slice(this.pos, this.bufferEnd);
+            let tokenType;
+
+            if (this._isAlpha(tokenValue.charAt(0))) {
+                tokenType = "variable";
+            } else if (tokenValue.charAt(0) === "\"" || tokenValue.charAt(0) === "'") {
+                tokenType = "string";
+                this.bufferEnd = phrase.indexOf(tokenValue.charAt(0), this.pos + 1);
+                if (this.bufferEnd === -1) throw "No close quote found";
+                tokenValue = phrase.slice(this.pos + 1, this.bufferEnd);
+            } else if (this._isSymbol(tokenValue.charAt(0))) {
+                tokenType = "operation";
+            } else if (this._isNumeric(tokenValue.charAt(0))) {
+                tokenType = "number";
+            }
+
+            if (tokenType) {
+                this.tokenizedJavaScript.push({
+                    token: tokenType,
+                    value: tokenValue
+                });
+            }
+
+            this.pos = this.bufferEnd + 1;
+        }
+    }
+
+    parseTokenedJavascript(code) {
+        code = this.tokenizedJavaScript || code;
+        let result = [];
+
+        for (let i = 0; i < code.length; i++) {
+            switch (code[i].token) {
+                case "variable":
+                    result.push("\"" + this.options[code[i].value] + "\"");
+                    break;
+                case "operation":
+                    result.push(code[i].value);
+                    break;
+                case "string":
+                    result.push("\"" + code[i].value + "\"");
+                    break;
+                case "number":
+                    result.push(code[i].value);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        this.reconstructedJavaScript = result.join("");
+        return this.reconstructedJavaScript;
+    }
+
+    _isAlpha(char) {
+        return ("a" <= char &&
+            "z" >= char) ||
+            ("A" <= char &&
+            "Z" >= char) ||
+            char === "_";
+    }
+
+    _isNumeric(char) {
+        return "0" <= char && "9" >= char;
+    }
+
+    _isAlphaNumeric(char) {
+        return this._isAlpha(char) || this._isNumeric(char);
+    }
+
+    _isSymbol(char) {
+        let symbols = ["+","-","*","/","="];
+        return symbols.includes(char);
+    }
+}
+
+function toggleJavaScript(currentChar, prevChar, isJavaScript) {
+    if (this.splitContent[i] === "|" && prevChar === "<") {
+        return true;
+    }
+    if (this.splitContent[i] === ">" && prevChar === "|") {
+        return false;
+    }
+    return isJavaScript;
 }
 
 module.exports = {
--- test/engine/lexer.js
+++ test/engine/lexer.js
@@ -6,7 +6,157 @@ const Lexer = require("../../engine/lexer");
 const HTMLResponseConstructor = require("../utility/htmlResponseConstructor");
 
 describe("Lexer::", () => {
-    it("Should load the basic page", (done) => {
-        return done();
+    describe("integration::", () => {
+        it("Should load the basic page", (done) => {
+            let lexer = Lexer.create({
+                content: "<div><| 2 + 2 |></div>",
+                options: {}
+            });
+
+            lexer.renderContent((err, result) => {
+                assert(!err, JSON.stringify(err));
+                expect(result).to.equal("<div>4</div>");
+                return done();
+            });
+        });
+        it("Should use a variable from options", (done) => {
+            let lexer = Lexer.create({
+                content: "<div><| hello + ' World' |></div>",
+                options: {
+                    hello: "Hello"
+                }
+            });
+
+            lexer.renderContent((err, result) => {
+                assert(!err, JSON.stringify(err));
+                expect(result).to.equal("<div>Hello World</div>");
+                return done();
+            });
+        });
+    });
+    describe("unit tests::", () => {
+        describe("parseJavaScript", () => {
+            it("should parse strings and variables", () => {
+                let lexer = Lexer.create({
+                    content: "<div><| hello + ' World' |></div>",
+                    options: {
+                        hello: "Hello",
+                        world: "World"
+                    }
+                });
+
+                expect(lexer.parseJavaScript("hello + ' ' + world")).to.equal("Hello World");
+            });
+            it("should parse addition", () => {
+                let lexer = Lexer.create({
+                    content: "<div><| 2 + 2 |></div>",
+                    options: {}
+                });
+
+                expect(lexer.parseJavaScript("2 + 2")).to.equal(4);
+            });
+        });
+        describe("tokenizeJavaScript", () => {
+            it("should tokenize simple JavaScript", () => {
+                let lexer = Lexer.create({
+                    content: "<div><| hello + ' World' |></div>",
+                    options: {
+                        hello: "Hello",
+                        world: "World"
+                    }
+                });
+                lexer.tokenizeJavaScript("hello + ' ' + world");
+
+                expect(lexer.tokenizedJavaScript).to.deep.equal(getHelloWorldToken());
+            });
+        });
+        describe("parseTokenedJavascript", () => {
+            it("should parse tokenized JavaScript", () => {
+                let lexer = Lexer.create({
+                    content: "<div><| hello + ' World' |></div>",
+                    options: {
+                        hello: "Hello",
+                        world: "World"
+                    }
+                });
+                let tokenizedJavaScript = getHelloWorldToken();
+
+                expect(lexer.parseTokenedJavascript(tokenizedJavaScript)).to.equal('"Hello"+" "+"World"');
+            });
+        });
+        it("_isAlpha", () => {
+            let lexer = Lexer.create({
+                content: "<div><| 2 + 2 |></div>",
+                options: {}
+            });
+
+            assert(lexer._isAlpha("a"), "a is being returned wrong");
+            assert(lexer._isAlpha("z"), "z is being returned wrong");
+            assert(lexer._isAlpha("A"), "A is being returned wrong");
+            assert(lexer._isAlpha("Z"), "Z is being returned wrong");
+            assert(lexer._isAlpha("_"), "_ is being returned wrong");
+            assert(lexer._isAlpha("f"), "f is being returned wrong");
+            assert(lexer._isAlpha("F"), "F is being returned wrong");
+            assert(!lexer._isAlpha("-"), "- is being returned wrong");
+            assert(!lexer._isAlpha("1"), "1 is being returned wrong");
+            assert(!lexer._isAlpha("0"), "0 is being returned wrong");
+            assert(!lexer._isAlpha("'"), "' is being returned wrong");
+        });
+        it("_isNumeric", () => {
+            let lexer = Lexer.create({
+                content: "<div><| 2 + 2 |></div>",
+                options: {}
+            });
+
+            assert(!lexer._isNumeric("a"), "a is being returned wrong");
+            assert(!lexer._isNumeric("A"), "A is being returned wrong");
+            assert(!lexer._isNumeric("Z"), "Z is being returned wrong");
+            assert(!lexer._isNumeric("_"), "_ is being returned wrong");
+            assert(!lexer._isNumeric("-"), "- is being returned wrong");
+            assert(lexer._isNumeric(9), "1 is being returned wrong");
+            assert(lexer._isNumeric(0), "0 is being returned wrong");
+            assert(lexer._isNumeric("5"), "5 is being returned wrong");
+        });
+        it("_isAlphaNumeric", () => {
+            let lexer = Lexer.create({
+                content: "<div><| 2 + 2 |></div>",
+                options: {}
+            });
+
+            assert(lexer._isAlphaNumeric("a"), "a is being returned wrong");
+            assert(lexer._isAlphaNumeric("A"), "A is being returned wrong");
+            assert(lexer._isAlphaNumeric("Z"), "Z is being returned wrong");
+            assert(lexer._isAlphaNumeric("_"), "_ is being returned wrong");
+            assert(!lexer._isAlphaNumeric("-"), "- is being returned wrong");
+            assert(lexer._isAlphaNumeric(9), "1 is being returned wrong");
+            assert(lexer._isAlphaNumeric(0), "0 is being returned wrong");
+            assert(lexer._isAlphaNumeric("5"), "5 is being returned wrong");
+            assert(!lexer._isAlphaNumeric("'"), "' is being returned wrong");
+        });
     });
 });
+
+function getHelloWorldToken() {
+    return [
+        {
+            token: "variable",
+            value: "hello"
+        },
+        {
+            token: "operation",
+            value: "+"
+        },
+        {
+            token: "string",
+            value: " "
+        },
+        {
+            token: "operation",
+            value: "+"
+        },
+        {
+            token: "variable",
+            value: "world"
+        }
+    ];
+}
